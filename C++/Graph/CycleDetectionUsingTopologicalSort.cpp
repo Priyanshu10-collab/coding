8885742a38fbff442efd62a43483d1880d88bfcb
Priyanshu10-collab <priyanshupandey1033@gmail.com>
2024-07-03 11:32:48 +0530

    Graph

---
+++ C++/Graph/CycleDetectionUsingTopologicalSort.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<unordered_map>
+#include<list>
+#include<queue>
+
+using namespace std;
+int detectCycleInDirectedGraph(int n, vector<pair<int, int>> &edges){
+      //create adj list
+    unordered_map<int,list<int>> adj;
+    for(int i=0;i<edges.size();i++){
+        int u = edges[i].first -1;
+        int v = edges[i].second -1;
+        adj[u].push_back(v);
+    }
+
+    //find all indegree
+    vector<int> indegree(n);
+    for(auto i: adj){
+        for(auto j: i.second){
+            indegree[j]++;
+        }
+    }
+
+    //0 indegree wala ko push kardo
+    queue<int>q;
+    for(int i=0;i<n;i++){
+        if(indegree[i] == 0)
+        q.push(i);
+    }
+
+    //do BFS
+    int cnt = 0;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+
+        //ans store
+        cnt++;
+
+        //neigbour indegree updates
+        for(auto neighbour : adj[front]){
+            indegree[neighbour]--;
+            if(indegree[neighbour]==0)
+            q.push(neighbour);
+        }
+    }
+    if(cnt == n)
+    return false;
+    else
+    return true;
+}
